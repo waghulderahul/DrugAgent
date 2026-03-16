a59463f39ca678d19a73b67ccf869088950321c7
rahulyuvraj <rahul.yuvraj@ayassbioscience.com>
2026-03-16 12:38:55 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,3 @@ setup(
     name="agentic_ai_wf",
     packages=find_packages(),
 )
-```
-
-Save. Then update the **Build Command** in Render settings to:
-```
-pip install -r requirements.txt && pip install -e .
